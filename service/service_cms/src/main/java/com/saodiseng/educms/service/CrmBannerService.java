cfb7a9320914f5b3a140aeb2352e1d1bd02ac5c9
SaoDiSeng <1300808192@qq.com>
2022-04-30 19:34:58 +0800

    cms模块代码生成器

---
+++ service/service_cms/src/main/java/com/saodiseng/educms/service/CrmBannerService.java
@@ -0,0 +1,16 @@
+package com.saodiseng.educms.service;
+
+import com.saodiseng.educms.entity.CrmBanner;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 首页banner表 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-04-30
+ */
+public interface CrmBannerService extends IService<CrmBanner> {
+
+}
---
+++ service/service_cms/src/main/java/com/saodiseng/educms/service/impl/CrmBannerServiceImpl.java
@@ -0,0 +1,20 @@
+package com.saodiseng.educms.service.impl;
+
+import com.saodiseng.educms.entity.CrmBanner;
+import com.saodiseng.educms.mapper.CrmBannerMapper;
+import com.saodiseng.educms.service.CrmBannerService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 首页banner表 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-04-30
+ */
+@Service
+public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
+
+}
