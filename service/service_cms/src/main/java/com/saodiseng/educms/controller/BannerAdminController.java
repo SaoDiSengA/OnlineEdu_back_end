2082815e22bee0b017a124df3a9df6cec366dc60
SaoDiSeng <1300808192@qq.com>
2022-04-30 20:15:47 +0800

    管理员端轮播图增删改查接口

---
+++ service/service_cms/src/main/java/com/saodiseng/educms/controller/BannerAdminController.java
@@ -0,0 +1,62 @@
+package com.saodiseng.educms.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.saodiseng.commonutils.R;
+import com.saodiseng.educms.entity.CrmBanner;
+import com.saodiseng.educms.service.CrmBannerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 后台banner管理接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-04-30
+ */
+@RestController
+@RequestMapping("/educms/banneradmin")
+@CrossOrigin
+public class BannerAdminController {
+    @Autowired
+    private CrmBannerService bannerService;
+
+    //1 分页查询banner
+    @GetMapping("pageBanner/{page}/limit")
+    public R pageBanner(@PathVariable long page, @PathVariable long limit){
+        Page<CrmBanner> pageBanner = new Page<>(page,limit);
+        bannerService.page(pageBanner,null);
+        return R.ok().data("items",pageBanner.getRecords()).data("total",pageBanner.getTotal());
+    }
+
+    // 2 添加banner
+    @PostMapping("addBanner")
+    public R addBanner(@RequestBody CrmBanner crmBanner){
+        bannerService.save(crmBanner);
+        return R.ok();
+    }
+
+    //获取Banner
+    @GetMapping("get/{id}")
+    public R get(@PathVariable String id){
+        CrmBanner banner = bannerService.getById(id);
+        return R.ok().data("item",banner);
+    }
+
+    // 3 修改banner
+    @PostMapping("updateBannerById")
+    public R updateBannerById(@RequestBody CrmBanner crmBanner){
+        bannerService.updateById(crmBanner);
+        return R.ok();
+    }
+
+    // 4 删除banner
+    @DeleteMapping("remove/{id}")
+    public R remove(@PathVariable String id){
+        bannerService.removeById(id);
+        return R.ok();
+    }
+}
+
