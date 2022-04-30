9645f5a71c6b3e13e385cf31267bf255c5b269b0
SaoDiSeng <1300808192@qq.com>
2022-04-30 20:15:22 +0800

    用户端轮播图显示接口，查询所有

---
+++ service/service_cms/src/main/java/com/saodiseng/educms/controller/BannerFrontController.java
@@ -0,0 +1,36 @@
+package com.saodiseng.educms.controller;
+
+import com.saodiseng.commonutils.R;
+import com.saodiseng.educms.entity.CrmBanner;
+import com.saodiseng.educms.service.CrmBannerService;
+import org.bouncycastle.cert.crmf.CertificateRequestMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 前台banner显示
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-04-30
+ */
+@RestController
+@RequestMapping("/educms/bannerfront")
+@CrossOrigin
+public class BannerFrontController {
+    @Autowired
+    private CrmBannerService bannerService;
+
+    // 查询所有banner
+    @GetMapping("getAllBanner")
+    public R getAllBanner(){
+        List<CrmBanner> list = bannerService.selectAllBanner();    //单独写service是为了redis方便，也可以直接用mybatis的方法
+        return R.ok();
+    }
+}
