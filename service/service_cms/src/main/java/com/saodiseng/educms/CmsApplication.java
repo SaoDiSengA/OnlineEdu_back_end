6b8d82f76436f556c05dd4b80a84243c356ecbed
SaoDiSeng <1300808192@qq.com>
2022-04-30 19:41:41 +0800

    cms相关设置

--- service/service_cms/src/main/java/com/saodiseng/educms/CmsApplication.java
+++ service/service_cms/src/main/java/com/saodiseng/educms/CmsApplication.java
@@ -1,5 +1,6 @@
 package com.saodiseng.educms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"com.saodiseng"})
 @SpringBootApplication
+@MapperScan("com.saodiseng.educms.mapper")
 public class CmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CmsApplication.class,args);
