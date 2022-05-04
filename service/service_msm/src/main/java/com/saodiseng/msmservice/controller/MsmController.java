66d8738c05517431ba2b71f082aeb28bc14a5fce
SaoDiSeng <1300808192@qq.com>
2022-05-04 19:22:39 +0800

    短信功能实现

--- service/service_msm/src/main/java/com/saodiseng/msmservice/controller/MsmController.java
+++ service/service_msm/src/main/java/com/saodiseng/msmservice/controller/MsmController.java
@@ -1,11 +1,47 @@
 package com.saodiseng.msmservice.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.saodiseng.commonutils.R;
+import com.saodiseng.commonutils.RandomUtil;
+import com.saodiseng.msmservice.service.MsmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/edumsm/msm")
 @CrossOrigin
 public class MsmController {
+
+    @Autowired
+    private MsmService msmService;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;     //用redis实现短信5分钟内有效
+
+    @GetMapping("send/{phone}")
+    public R sendMsm(@PathVariable String phone){
+        //1 从redis获取验证码，如果获取到直接返回
+        String code = redisTemplate.opsForValue().get(phone);
+        if (!StringUtils.isEmpty(code)) {
+            return R.ok();
+        }
+        //2 如果redis获取不到，进行阿里云发送
+        //验证码这个值不是阿里云生成的，需要自己生成
+        code = RandomUtil.getFourBitRandom();
+        Map<String,Object> param = new HashMap<>();
+        param.put("code",code);
+        boolean isSend = msmService.send(param,phone);
+        if (isSend) {
+            //发送成功，把发送成功的验证码放到redis里面去
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+            return R.ok();
+        } else {
+            return R.error().message("短信发送失败");
+        }
+    }
 }
