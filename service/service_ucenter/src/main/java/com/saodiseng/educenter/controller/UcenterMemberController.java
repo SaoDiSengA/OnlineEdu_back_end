323eed209da29764172eb313e8bfa405524a3a89
SaoDiSeng <1300808192@qq.com>
2022-05-10 18:48:02 +0800

    根据token获取用户信息

--- service/service_ucenter/src/main/java/com/saodiseng/educenter/controller/UcenterMemberController.java
+++ service/service_ucenter/src/main/java/com/saodiseng/educenter/controller/UcenterMemberController.java
@@ -1,12 +1,15 @@
 package com.saodiseng.educenter.controller;
 
 
+import com.saodiseng.commonutils.JwtUtils;
 import com.saodiseng.commonutils.R;
 import com.saodiseng.educenter.entity.UcenterMember;
 import com.saodiseng.educenter.entity.vo.RegisterVo;
 import com.saodiseng.educenter.service.UcenterMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -38,6 +41,16 @@ public class UcenterMemberController {
     public R registerUser(@RequestBody RegisterVo registerVo){
         memberService.register(registerVo);
         return R.ok();
+    }
+
+    //根据token获取用户信息
+    @GetMapping("getUserInfo")
+    public R getUserInfo(HttpServletRequest request){
+        //调用jwt工具类根据request对象返回id
+        String memberId = JwtUtils.getMemberIdByJwtToken(request);
+        //根据id获取信息
+        UcenterMember member = memberService.getById(memberId);
+        return R.ok().data("userInfo",member);
     }
 }
 
