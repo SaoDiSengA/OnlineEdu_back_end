c564eba2ab08294416016a8a6e46f7bd5b2b583e
SaoDiSeng <1300808192@qq.com>
2022-05-21 22:39:09 +0800

    order模块

---
+++ service/service_order/src/main/java/com/saodiseng/eduorder/OrdersApplication.java
@@ -0,0 +1,13 @@
+package com.saodiseng.eduorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.saodiseng"})
+public class OrdersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApplication.class,args);
+    }
+}
