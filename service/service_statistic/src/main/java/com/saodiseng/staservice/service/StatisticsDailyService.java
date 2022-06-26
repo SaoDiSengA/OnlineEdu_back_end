ca7d6b6768c92a360bfa90cbca0704981284cb0b
SaoDiSeng <1300808192@qq.com>
2022-06-26 16:23:59 +0800

    统计模块初始化

---
+++ service/service_statistic/src/main/java/com/saodiseng/staservice/service/StatisticsDailyService.java
@@ -0,0 +1,16 @@
+package com.saodiseng.staservice.service;
+
+import com.saodiseng.staservice.entity.StatisticsDaily;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 网站统计日数据 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-06-26
+ */
+public interface StatisticsDailyService extends IService<StatisticsDaily> {
+
+}
---
+++ service/service_statistic/src/main/java/com/saodiseng/staservice/service/impl/StatisticsDailyServiceImpl.java
@@ -0,0 +1,20 @@
+package com.saodiseng.staservice.service.impl;
+
+import com.saodiseng.staservice.entity.StatisticsDaily;
+import com.saodiseng.staservice.mapper.StatisticsDailyMapper;
+import com.saodiseng.staservice.service.StatisticsDailyService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 网站统计日数据 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-06-26
+ */
+@Service
+public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMapper, StatisticsDaily> implements StatisticsDailyService {
+
+}
