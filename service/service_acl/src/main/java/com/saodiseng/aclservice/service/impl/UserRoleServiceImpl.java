1ff32af454897806c45fe4ac8c83e8615ae1de46
SaoDiSeng <1300808192@qq.com>
2022-07-04 23:39:47 +0800

    security模块添加，acl模块

---
+++ service/service_acl/src/main/java/com/saodiseng/aclservice/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.saodiseng.aclservice.service.impl;
+
+import com.saodiseng.aclservice.entity.UserRole;
+import com.saodiseng.aclservice.mapper.UserRoleMapper;
+import com.saodiseng.aclservice.service.UserRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
